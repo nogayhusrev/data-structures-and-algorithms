5c9336544368f06fe91c729abd69f36873c48298
nogayhusrev <nogayhusrev@gmail.com>
2023-04-21 01:28:15 +0300

    Module 2 Linked List done

---
+++ m2-linked-lists/src/main/java/com/nogayhusrev/LinkedList.java
@@ -0,0 +1,224 @@
+package com.nogayhusrev;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+
+        if (isEmpty()) {
+            first = last = node;
+        }
+
+        node.next = first;
+        first = node;
+        size++;
+    }
+
+    public void addLast(int item) {
+
+        var node = new Node(item);
+
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            last.next = node;
+            last = node;
+        }
+        size++;
+
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item)
+                return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    public void removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+
+        } else {
+            var second = first.next;
+            first.next = null;
+            first = second;
+
+        }
+
+
+        size--;
+    }
+
+    public void removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+        } else {
+            var previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+
+        size--;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    private Node getPrevious(Node node) {
+        var current = first;
+        while (current != null) {
+            if (current.next == node)
+                break;
+            current = current.next;
+        }
+
+        return current;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int[] toArray() {
+
+        int[] array = new int[size];
+        var current = first;
+        var index = 0;
+        while (current != null) {
+            array[index++] = current.value;
+            current = current.next;
+        }
+        return array;
+    }
+
+    public void reverse() {
+        if (isEmpty())
+            return;
+
+        var previous = first;
+        var current = first.next;
+
+        while (current != null) {
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        last = first;
+        last.next = null;
+        first = previous;
+
+
+    }
+
+    public int getKthFromTheEnd(int k) {
+
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        var a = first;
+        var b = first;
+
+        for (int i = 0; i < k - 1; i++) {
+            b = b.next;
+            if (b == null)
+                throw new IllegalArgumentException();
+        }
+
+        while (b != last) {
+            a = a.next;
+            b = b.next;
+        }
+
+        return a.value;
+    }
+
+    public void printMiddle() {
+
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        var a = first;
+        var b = first;
+
+        while (b != last && b.next != last) {
+            a = a.next;
+            b = b.next.next;
+        }
+
+        if (b == last)
+            System.out.println(a.value);
+        else
+            System.out.println(a.value + ", " + a.next.value);
+    }
+
+    public boolean hasLoop() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        var slow = first;
+        var fast = first;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
+        return false;
+
+
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ m2-linked-lists/src/main/java/com/nogayhusrev/Main.java
@@ -0,0 +1,27 @@
+package com.nogayhusrev;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new LinkedList();
+
+        list.addLast(12);
+        list.addLast(13);
+        list.addLast(14);
+        list.addLast(143);
+        list.addLast(144);
+        list.addLast(1442);
+
+//        System.out.println(list.getKthFromTheEnd(-5));
+
+        System.out.println(list.hasLoop());
+
+        list.printMiddle();
+
+        int[] aa = list.toArray();
+
+        System.out.println(Arrays.toString(aa));
+
+    }
+}
