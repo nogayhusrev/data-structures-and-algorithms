c5b68c7a83553d54278cb3c5d1e1b5b058d1f793
nogayhusrev <nogayhusrev@gmail.com>
2023-06-11 18:27:10 +0300

    trees

--- m6-trees/src/main/java/com/nogayhusrev/Tree.java
+++ m6-trees/src/main/java/com/nogayhusrev/Tree.java
@@ -4,6 +4,45 @@ public class Tree {
 
     private Node root;
 
+    public void insert(int value){
+        var node = new Node(value);
+
+        if (root == null){
+            root = node;
+            return;
+        }
+
+        var curr = root;
+        while (true){
+            if (value < curr.value){
+                if (curr.leftChild == null){
+                    curr.leftChild = node;
+                    return;
+                }
+                curr = curr.leftChild;
+            }else {
+                if (curr.rightChild == null){
+                    curr.rightChild = node;
+                    return;
+                }
+                curr = curr.rightChild;
+            }
+        }
+    }
+
+    public boolean find(int value){
+        var current = root;
+        while (current != null) {
+            if (value < current.value)
+                current = current.leftChild;
+            else if (value > current.value)
+                current = current.rightChild;
+            else
+                return true;
+        }
+        return false;
+    }
+
 
 
 
