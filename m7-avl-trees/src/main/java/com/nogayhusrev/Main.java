ed31d5b7ef6819b2b86116bc8301fd2ad314c6a0
nogayhusrev <nogayhusrev@gmail.com>
2023-06-13 11:42:34 +0300

    avl tree

---
+++ m7-avl-trees/src/main/java/com/nogayhusrev/Main.java
@@ -0,0 +1,14 @@
+package com.nogayhusrev;
+
+public class Main {
+    public static void main(String[] args) {
+        AvlTree avlTree = new AvlTree();
+        avlTree.insert(7);
+        avlTree.insert(8);
+        avlTree.insert(9);
+
+
+
+
+    }
+}
