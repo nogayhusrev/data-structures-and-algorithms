5d0e88940e53a4857dba0ca7e10f8cb35b8d6020
nogayhusrev <nogayhusrev@gmail.com>
2023-08-08 16:20:21 +0300

    SortingUtil done

---
+++ m12-sorting-algorithms/src/main/java/com/nogayhusrev/Main.java
@@ -0,0 +1,7 @@
+package com.nogayhusrev;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {12, 342, 53, 231, 7323, 123, 8324, 123, 311, 1, -12};
+    }
+}
---
+++ m12-sorting-algorithms/src/main/java/com/nogayhusrev/SortingUtil.java
@@ -0,0 +1,153 @@
+package com.nogayhusrev;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingUtil {
+
+
+    /******************* Bubble Sort ********************/
+    public void bubbleSort(int[] array) {
+        boolean isSorted;
+        for (var i = 0; i < array.length; i++) {
+            isSorted = true;
+            for (var j = 1; j < array.length - i; j++)
+                if (array[j] < array[j - 1]) {
+                    swap(array, j, j - 1);
+                    isSorted = false;
+                }
+            if (isSorted)
+                return;
+        }
+    }
+
+    /******************* Selection Sort ********************/
+    public void selectionSort(int[] array) {
+        for (var i = 0; i < array.length; i++) {
+            var minIndex = i;
+            for (var j = i; j < array.length; j++)
+                if (array[j] < array[minIndex])
+                    minIndex = j;
+            swap(array, minIndex, i);
+        }
+    }
+
+    /******************* Insertion Sort ********************/
+    public void insertionSort(int[] array) {
+        for (var i = 1; i < array.length; i++) {
+            var current = array[i];
+            var j = i - 1;
+            while (j >= 0 && array[j] > current) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
+        }
+    }
+
+    /******************* Merge Sort ********************/
+    public void mergeSort(int[] array) {
+        if (array.length < 2)
+            return;
+
+        var middle = array.length / 2;
+
+        int[] left = new int[middle];
+        for (var i = 0; i < middle; i++)
+            left[i] = array[i];
+
+        int[] right = new int[array.length - middle];
+        for (var i = middle; i < array.length; i++)
+            right[i - middle] = array[i];
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(left, right, array);
+    }
+
+    private void merge(int[] left, int[] right, int[] result) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j])
+                result[k++] = left[i++];
+            else
+                result[k++] = right[j++];
+        }
+
+        while (i < left.length)
+            result[k++] = left[i++];
+
+        while (j < right.length)
+            result[k++] = right[j++];
+    }
+
+    /******************* Quick Sort ********************/
+    public void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private void quickSort(int[] array, int start, int end) {
+        if (start >= end)
+            return;
+
+        var boundary = partition(array, start, end);
+
+        quickSort(array, start, boundary - 1);
+        quickSort(array, boundary + 1, end);
+    }
+
+    private int partition(int[] array, int start, int end) {
+        var pivot = array[end];
+        var boundary = start - 1;
+        for (var i = start; i <= end; i++)
+            if (array[i] <= pivot)
+                swap(array, i, ++boundary);
+
+        return boundary;
+    }
+
+    /******************* Counting Sort ********************/
+    public void sort(int[] array, int max) {
+        int[] counts = new int[max + 1];
+        for (var item : array)
+            counts[item]++;
+
+        var k = 0;
+        for (var i = 0; i < counts.length; i++)
+            for (var j = 0; j < counts[i]; j++)
+                array[k++] = i;
+    }
+
+    /******************* Bucket Sort ********************/
+    public void bucketSort(int[] array, int numberOfBuckets) {
+        var i = 0;
+        for (var bucket : createBuckets(array, numberOfBuckets)) {
+            Collections.sort(bucket);
+            for (var item : bucket)
+                array[i++] = item;
+        }
+    }
+
+    private List<List<Integer>> createBuckets(int[] array, int numberOfBuckets) {
+        List<List<Integer>> buckets = new ArrayList<>();
+        for (var i = 0; i < numberOfBuckets; i++)
+            buckets.add(new ArrayList<>());
+
+        for (var item : array)
+            buckets.get(item / numberOfBuckets).add(item);
+
+        return buckets;
+    }
+
+
+    private void swap(int[] array, int index1, int index2) {
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+
+}
